c39ee20458714a683809ec2699a4b65711410670
Unknown <veyselakpinar13@gmail.com>
2019-04-09 09:44:54 +0300

    configuration

---
+++ MyFramework.Business/ServiceContracts/Wcf/IProductDetailService.cs
@@ -0,0 +1,18 @@
+﻿using MyFramework.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFramework.Business.ServiceContracts.Wcf
+{
+    [ServiceContract]
+  public  interface IProductDetailService
+    {
+        [OperationContract]
+        List<Product> GetAll();
+
+    }
+}
